efb361db5b2c3c1e371dd77310050950b9558258
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-03-14 12:21:38 +0100

    used repo and entity manager

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,9 +12,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/recettes', name: 'recipe.index')]
-    public function index(Request $request, RecipeRepository $repository): Response
+    public function index(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
     {
-        $recipes = $repository->findAll();
+        $recipes = $repository->findWithDurationLowerThan(10);
+        $recipes[0]->setTitle('Pâtes bolognaise');
+        $em->flush();
         return $this->render('recipe/index.html.twig', [
             'recipes' => $recipes
         ]);
--- src/Repository/RecipeRepository.php
+++ src/Repository/RecipeRepository.php
@@ -21,6 +21,22 @@ class RecipeRepository extends ServiceEntityRepository
         parent::__construct($registry, Recipe::class);
     }
 
+    /**
+     * 
+     * @param int $duration 
+     * @return array Recipe[]
+     */
+    public function findWithDurationLowerThan(int $duration): array
+    {
+        return $this->createQueryBuilder('r')
+                    ->where('r.duration <= :duration')
+                    ->orderBy('r.duration', 'ASC')
+                    ->setMaxResults(20)
+                    ->setParameter('duration', $duration)
+                    ->getQuery()
+                    ->getResult();
+    }
+
     //    /**
     //     * @return Recipe[] Returns an array of Recipe objects
     //     */
