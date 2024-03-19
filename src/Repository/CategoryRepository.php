4b622880a07493c0f8fa49719d604fe479eb9fe0
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-03-19 19:22:40 +0100

    add: DTO

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -21,10 +21,13 @@ class CategoryRepository extends ServiceEntityRepository
         parent::__construct($registry, Category::class);
     }
 
+    /**
+     * @return CategoryWithCountDTO[]
+     */
     public function findAllWithCount(): array
     {
         return $this->createQueryBuilder('c')
-            ->select('c as category', 'COUNT(c.id) as total')
+            ->select('NEW App\\DTO\\CategoryWithCountDTO(c.id, c.name, COUNT(c.id))')
             ->leftJoin('c.recipes', 'r')
             ->groupBy('c.id')
             ->getQuery()
